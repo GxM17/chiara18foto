cc53463bc3d338295a14faf5f237da35045fd09e
GxM17 <mfurcas@gmail.com>
2026-06-16 07:37:55 +0200

    Delete src directory

--- src/pages/AuthCallback.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-export default function AuthCallback() {
-  const navigate = useNavigate()
-  useEffect(() => { navigate('/dashboard', { replace: true }) }, [])
-  return null
-}
